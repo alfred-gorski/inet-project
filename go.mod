bb51e98edcfd8f8457dc3f09239498c98fd3f133
alfred-gorksi <dzhongzhu@icloud.com>
2021-03-14 01:15:14 +0100

    dep[fiber]: update to v2

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/alfred-gorski/inet-project
 go 1.16
 
 require (
-	github.com/gofiber/fiber v1.14.6
-	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/gofiber/fiber/v2 v2.5.0
+	github.com/klauspost/compress v1.11.12 // indirect
+	github.com/valyala/fasthttp v1.22.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/sys v0.0.0-20210313202042-bd2e13477e9c // indirect
 )
--- main.go
+++ main.go
@@ -3,7 +3,8 @@ package main
 import (
 	"log"
 
-	"github.com/gofiber/fiber"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 )
 
 func main() {
@@ -13,24 +14,26 @@ func main() {
 	// serve Single Page application on "/public"
 	// assume static file at dist folder
 	app.Static("/", "public")
+	app.Use(recover.New())
 
 	apiGroup := app.Group("/api")
 	{
-		apiGroup.Get("/user", func(c *fiber.Ctx) {
-			c.JSON(fiber.Map{"id": 1, "name": "kiyon"})
+		apiGroup.Get("/user", func(c *fiber.Ctx) error {
+			return c.JSON(fiber.Map{"id": 1, "name": "kiyon"})
 		})
 	}
 
-	app.Get("/*", func(c *fiber.Ctx) {
-		if err := c.SendFile("public/index.html"); err != nil {
-			c.Next(fiber.ErrInternalServerError)
-		}
+	app.Get("/*", func(c *fiber.Ctx) error {
+		// if err := c.SendFile("public/index.html"); err != nil {
+		// 	c.Next(fiber.ErrInternalServerError)
+		// }
+		return c.SendFile("public/index.html")
 	})
 
-	for _, route := range app.Routes() {
-		log.Println(route.Method, route.Path)
-	}
+	// for _, route := range app.Routes() {
+	// 	log.Println(route.Method, route.Path)
+	// }
 
 	// Start server on http://localhost:3000
-	log.Fatal(app.Listen(3000))
+	log.Fatal(app.Listen(":3000"))
 }
