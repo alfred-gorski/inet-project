7a4d5978a7564e791b8491216625c824e94ef5ca
alfred-gorksi <dzhongzhu@icloud.com>
2021-03-16 16:40:07 +0100

    feat: toolbar

--- web/src/app/material/material.module.ts
+++ web/src/app/material/material.module.ts
@@ -2,19 +2,19 @@ import { NgModule } from '@angular/core';
 
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 const MaterialComponents = [
-  MatButtonModule
+  MatButtonModule,
+  MatToolbarModule,
+  MatIconModule
 ];
 
 @NgModule({
   declarations: [],
-  imports: [
-    MaterialComponents
-  ],
-  exports:[
-    MaterialComponents
-  ]
-  
+  imports: [MaterialComponents],
+  exports: [MaterialComponents]
+
 })
 export class MaterialModule { }
