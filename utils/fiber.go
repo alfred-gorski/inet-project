3066993a81cd82424429f470f0a3cb68dea99ddc
alfred-gorksi <dzhongzhu@icloud.com>
2021-03-25 14:14:03 +0100

    fix[utils]: get userid

--- utils/fiber.go
+++ utils/fiber.go
@@ -1,9 +1,16 @@
 package utils
 
-import "github.com/gofiber/fiber/v2"
+import (
+	jwt "github.com/form3tech-oss/jwt-go"
+	"github.com/gofiber/fiber/v2"
+)
 
 // GetUserID is helper function for getting authenticated user's id
 func GetUserID(c *fiber.Ctx) uint {
-	id, _ := c.Locals("userID").(uint)
-	return id
+	user := c.Locals("user").(*jwt.Token)
+	claims := user.Claims.(jwt.MapClaims)
+	id := claims["ID"].(float64)
+	return uint(id)
+	// id, _ := c.Locals("userID").(uint)
+	// return id
 }
