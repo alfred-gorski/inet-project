4ef4e912256b733a2d296b0555ce4e9b62a69371
alfred-gorksi <dzhongzhu@icloud.com>
2021-03-25 12:47:01 +0100

    rename USER to userID

--- utils/fiber.go
+++ utils/fiber.go
@@ -4,6 +4,6 @@ import "github.com/gofiber/fiber/v2"
 
 // GetUserID is helper function for getting authenticated user's id
 func GetUserID(c *fiber.Ctx) uint {
-	id, _ := c.Locals("token").(uint)
+	id, _ := c.Locals("userID").(uint)
 	return id
 }
